8ae4eca1203e027e19b1e172b362128e6037550e
Everfighting <cbb903601682@163.com>
2016-06-26 16:50:23 +0800

    rename

--- 4.2.py
+++
@@ -1,4 +0,0 @@
-# coding=utf-8
-i = 5
-print 'I repeat, the value is', i
-# 若前面不小心有空格则会引发错误
--- 7.1.py
+++
@@ -1,7 +0,0 @@
-# coding=utf-8
-#!/usr/bin/python
-def sayHello():
-	print 'Hello World!'
-# block belonging to the function
-
-sayHello() # call the function
