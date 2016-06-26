c426b5cedb23d3b678f7423aeba3f9074ff3f010
Everfighting <cbb903601682@163.com>
2016-06-26 15:36:01 +0800

    chapter6~chapter7

---
+++ 6.4.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+#!/usr/bin/python
+while True:
+	s = raw_input('Enter something : ')
+	if s == 'quit':
+		break
+	print 'Length of the string is', len(s)
+
+print 'Done'
+
+# 通过quit循环到达程序结尾来停止程序
+# 
+#
---
+++ 7.4.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+#!/usr/bin/python
+
+def func():
+	global x
+	print 'x is', x
+
+x = 2
+print 'Changed local x to', x
+
+x = 50
+func()
+print 'Value of x is', x
