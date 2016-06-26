061e992d67981efa642e5d7acf191f301c2c2d08
Everfighting <cbb903601682@163.com>
2016-06-26 09:09:34 +0800

    chapter1~chapter5

---
+++ 4.1.py
@@ -0,0 +1,8 @@
+# -*- coding=utf-8 -*-
+# 一个物理行中使用多个逻辑行
+i = 5 ;
+print i
+# 多物理行表示单逻辑行
+s = 'This is a string. \
+This continues the string.'
+print s
---
+++ 4.2.py
@@ -0,0 +1,4 @@
+# coding=utf-8
+i = 5
+print 'I repeat, the value is', i
+# 若前面不小心有空格则会引发错误
